32bae4bf93f403978c7e0467895c3d248d53ba8e
tandori46001 <tandori46001@outlook.com>
2021-01-07 22:37:22 -0500

    initial

--- techno/scripts/vmcreate-ubuntu_min.sh
+++ techno/scripts/vmcreate-ubuntu_min.sh
@@ -5,7 +5,7 @@
 
 # declare variables
 PATHOVFTOOL="/Users/na/_git/wiki/techno/ansible/play-linux-new-server/files/VMwareOVFTool"
-OVAPATH="/Users/na/Downloads/_vmware"
+OVAPATH="/Users/na/Downloads/_vmware/###TEMPLATE###"
 ESXSRV="10.0.99.5"
 DATASTORENAME="DS01_VMFS_SSD250GB"
 DISKTYPE="thin"
@@ -23,17 +23,47 @@ NOMBREVMUBUNTU='3'
 VMNAMEUBUNTUSRVMIN="lubusrvlabotest"    # mon debut pour multiples VMs
 #-------------------------------------------
 
+#-------------------------------------------
+# informations sur des centos server minimal
+OVACENTOS7SRVMIN="centos7-minimal.ova"
+OVACENTOS8SRVMIN="centos8-minimal.ova"
+NOMBREVMCENTOS7='2'
+NOMBREVMCENTOS8='2'
+
+VMNAMECENTOS7SRVMIN="lcent7srvlabotest"    # mon debut pour multiples VMs
+VMNAMECENTOS8SRVMIN="lcent8srvlabotest"    # mon debut pour multiples VMs
+#-------------------------------------------
+
+
 echo "enter user name : "
 read -s USERNAME
 
 echo "enter user password : "
 read -s USERPASSWORD
 
-# create vm ubuntu-minimal LABO
+# create "one" vm ubuntu-minimal LABO
 # $PATHOVFTOOL/ovftool --disableVerification --noSSLVerify -ds=$DATASTORENAME -dm=$DISKTYPE -n="$VMNAMEUBUNTUSRVMIN1" --network=$VMNETWORKLABO1 $POWERVMON $OVAPATH/$OVAUBUNTUSRVMIN vi://$USERNAME:$USERPASSWORD@$ESXSRV
 
-# create LABO, multiples vm
-for (( i=1; i<=$NOMBREVMUBUNTU; i++ ))
+# create "one" vm centos7-minimal LABO
+# $PATHOVFTOOL/ovftool --disableVerification --noSSLVerify -ds=$DATASTORENAME -dm=$DISKTYPE -n="$VMNAMECENTOS7SRVMIN" --network=$VMNETWORKLABO1 $POWERVMON $OVAPATH/$OVACENTOS7SRVMIN vi://$USERNAME:$USERPASSWORD@$ESXSRV
+
+# create "one" vm centos8-minimal LABO
+# $PATHOVFTOOL/ovftool --disableVerification --noSSLVerify -ds=$DATASTORENAME -dm=$DISKTYPE -n="$VMNAMECENTOS8SRVMIN" --network=$VMNETWORKLABO1 $POWERVMON $OVAPATH/$OVACENTOS8SRVMIN vi://$USERNAME:$USERPASSWORD@$ESXSRV
+
+# create LABO - UBUNTU servers, multiples vm
+#for (( i=1; i<=$NOMBREVMUBUNTU; i++ ))
+#do
+#  $PATHOVFTOOL/ovftool --disableVerification --noSSLVerify -ds=$DATASTORENAME -dm=$DISKTYPE -n="$VMNAMEUBUNTUSRVMIN$i" --network=$VMNETWORKLABO1 $POWERVMON $OVAPATH/$OVAUBUNTUSRVMIN vi://$USERNAME:$USERPASSWORD@$ESXSRV
+#done
+
+# create LABO - CENTOS7 servers, multiples vm
+for (( i=1; i<=$NOMBREVMCENTOS7; i++ ))
 do
-  $PATHOVFTOOL/ovftool --disableVerification --noSSLVerify -ds=$DATASTORENAME -dm=$DISKTYPE -n="$VMNAMEUBUNTUSRVMIN$i" --network=$VMNETWORKLABO1 $POWERVMON $OVAPATH/$OVAUBUNTUSRVMIN vi://$USERNAME:$USERPASSWORD@$ESXSRV
+  $PATHOVFTOOL/ovftool --disableVerification --noSSLVerify -ds=$DATASTORENAME -dm=$DISKTYPE -n="$VMNAMECENTOS7SRVMIN$i" --network=$VMNETWORKLABO1 $POWERVMON $OVAPATH/$OVACENTOS7SRVMIN vi://$USERNAME:$USERPASSWORD@$ESXSRV
+done
+
+# create LABO - CENTOS8 servers, multiples vm
+for (( i=1; i<=$NOMBREVMCENTOS8; i++ ))
+do
+  $PATHOVFTOOL/ovftool --disableVerification --noSSLVerify -ds=$DATASTORENAME -dm=$DISKTYPE -n="$VMNAMECENTOS8SRVMIN$i" --network=$VMNETWORKLABO1 $POWERVMON $OVAPATH/$OVACENTOS8SRVMIN vi://$USERNAME:$USERPASSWORD@$ESXSRV
 done
