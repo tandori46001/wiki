1d34986340eed79f5e789bd5f9e551a517707693
tandori46001 <tandori46001@outlook.com>
2021-01-16 13:58:09 -0500

    initial

--- techno/scripts/export-vm-to-ovf.sh
+++ techno/scripts/export-vm-to-ovf.sh
@@ -4,7 +4,7 @@ PATHOVFTOOL="/Users/na/_git/wiki/techno/ansible/play-linux-new-server/files/VMwa
 PATHDEST="/Users/na/Downloads/_vmware/images-ovf"
 ESXSRV="192.168.60.5"
 # VMSRVNAME="lubudsk01"
-VMSRVNAME="cent8tem"
+# VMSRVNAME="cent8tem"
 # VMSRVNAME="vmlubuxx"
 
 # export VM guest from ESXi to local PC
@@ -12,4 +12,4 @@ VMSRVNAME="cent8tem"
 # $PATHOVFTOOL/ovftool vi://$ESXSRV/$VMSRVNAME "$PATHDEST/$VMSRVNAME.ovf"
 #
 #---OVA
-$PATHOVFTOOL/ovftool vi://$ESXSRV/$VMSRVNAME "$PATHDEST/$VMSRVNAME.ova"
+# $PATHOVFTOOL/ovftool vi://$ESXSRV/$VMSRVNAME "$PATHDEST/$VMSRVNAME.ova"
