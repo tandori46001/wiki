39fb06690f585f6813525bd99b24aef48f4a6c72
tandori46001 <tandori46001@outlook.com>
2021-01-15 23:42:15 -0500

    initial

--- techno/scripts/export-vm-to-ovf.sh
+++ techno/scripts/export-vm-to-ovf.sh
@@ -4,7 +4,7 @@ PATHOVFTOOL="/Users/na/_git/wiki/techno/ansible/play-linux-new-server/files/VMwa
 PATHDEST="/Users/na/Downloads/_vmware/images-ovf"
 ESXSRV="192.168.60.5"
 # VMSRVNAME="lubudsk01"
-VMSRVNAME="lcent7srvlabotestxx"
+VMSRVNAME="cent7tem"
 # VMSRVNAME="vmlubuxx"
 
 # export VM guest from ESXi to local PC
